7310af128fc38e4ee1541f20502a65167c41597c
Bruce Parrello <bparrello@figresearch.com>
2015-12-16 14:57:38 -0600

    Added prototype.

---
+++ scripts/svc_proto.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2003-2015 University of Chicago and Fellowship
+# for Interpretations of Genomes. All Rights Reserved.
+#
+# This file is part of the SEED Toolkit.
+#
+# The SEED Toolkit is free software. You can redistribute
+# it and/or modify it under the terms of the SEED Toolkit
+# Public License.
+#
+# You should have received a copy of the SEED Toolkit Public License
+# along with this program; if not write to the University of Chicago
+# at info@ci.uchicago.edu or the Fellowship for Interpretation of
+# Genomes at veronika@thefig.info or download a copy from
+# http://www.theseed.org/LICENSE.TXT.
+#
+
+
+use strict;
+use warnings;
+use ServicesUtils;
+
+=head1 Common Services Function
+
+    svc_proto.pl [ options ] parm1 parm2 ...
+
+## detailed description
+
+=head2 Parameters
+
+See L<ServicesUtils> for more information about common command-line options.
+
+## describe positional parameters
+
+=over 4
+
+## describe command-line options
+
+=back
+
+=cut
+
+# Get the command-line parameters.
+my ($opt, $helper) = ServicesUtils::get_options('type');
+# Open the input file.
+my $ih = ServicesUtils::ih($opt);
+# Loop through it.
+while (my @batch = ServicesUtils::get_batch($ih, $opt)) {
+    my $resultsH = $helper->proto([map { $_->[0] } @batch]); ## TODO fix this line
+    # Output the batch.
+    for my $couplet (@batch) {
+        # Get the input value and input row.
+        my ($value, $row) = @$couplet;
+        # Loop through the input value's results;
+        my $results = $resultsH->{$value};
+        for my $result (@$results) {
+            # Output this result with the original row.
+            print join("\t", @$row, $result), "\n";
+        }
+    }
+}
