0251d0c0622a7998df0c657c973d2ba773df4739
disz <disz@mcs.anl.gov>
2015-12-17 14:16:47 -0600

    Function Id to features

--- lib/STKServices.pm
+++ lib/STKServices.pm
@@ -203,6 +203,46 @@ sub role_to_features {
     return \%retVal;
 }
 
+
+=head3 function_to_features
+
+    my $featureHash = $helper->features_of(\@functionIDs, $priv);
+
+Return a hash mapping each incoming function ID to a list of its feature IDs.
+
+=over 4
+
+=item roleIDs
+
+A reference to a list of IDs for the functions to be processed.
+
+=item priv
+
+The privilege level for the relevant assignments.
+
+=item RETURN
+
+Returns a reference to a hash mapping each incoming role ID to a list reference containing all the features with that
+role.
+
+=back
+
+=cut
+
+sub function_to_features {
+    my ($self, $functionIDs, $priv) = @_;
+    my $shrub = $self->{shrub};
+    my %retVal;
+    for my $funcid (@$functionIDs) {
+        # Get the IDs of the desired features.
+        my @funcids = $shrub->GetFlat('Function2Feature',
+                'Function2Feature(from-link) = ? AND Function2Feature(security) = ?', [$funcid, $priv], 'Function2Feature(to-link)');
+        # Store the returned features with the function ID.
+        $retVal{$funcid} = \@funcids;
+    }
+    return \%retVal;
+}
+
 =head3 role_to_ss
 
     my $ssHash = $helper->role_to_ss(\@roles, $priv);
