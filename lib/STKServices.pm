56af89a0ce874e38d862700a9012b38a18f1bc73
Ross Overbeek <RossOverbeek@gmail.com>
2015-12-21 12:34:29 -0600

    bad

--- lib/STKServices.pm
+++ lib/STKServices.pm
@@ -21,6 +21,7 @@ package STKServices;
     use strict;
     use warnings;
     use Shrub;
+    use Data::Dumper;
 
 =head1 SEEDtk Services Helper
 
@@ -864,9 +865,92 @@ sub roles_in_genomes {
         my @roleIDs = $shrub->GetFlat('Genome2Feature Feature Feature2Function Function Function2Role',
                 'Genome2Feature(from-link) = ? AND Feature2Function(security) = ?', [$gid,2], 'Function2Role(to-link)');
         # Store the returned role IDs for the genome ID.
-        $retVal{$gid} = \@roleIDs;
+	my %uniq = map { ($_ => 1) } @roleIDs;
+        $retVal{$gid} = [sort keys(%uniq)];
     }
     return \%retVal;
 }
 
+=head3 pegs_implementing_roles_in_genome
+
+    my $roleHash = $helper->pegs_implementing_roles_in_genome($genome,\@roleIDs);
+
+Return a hash mapping each incoming role ID to a list of PEGs from $g that implement the role.
+
+=over 4
+
+=item roleIDs
+
+A reference to a list of IDs of the roles to be processed.
+
+=item RETURN
+
+Returns a reference to a hash mapping each incoming role ID to a list reference containing all the 
+PEGs implementing the role in the given genome.
+
+=back
+
+=cut
+
+sub pegs_implementing_roles_in_genome {
+    my ($self, $g,$role_ids) = @_;
+    my $shrub = $self->{shrub};
+    my %retVal;
+
+    # Break the input list into batches and retrieve a batch at a time.
+    my $start = 0;
+    while ($start < @$role_ids) {
+        # Get this chunk.
+        my $end = $start + 10;
+        if ($end >= @$role_ids) {
+            $end = @$role_ids - 1;
+        }
+        my @slice = @{$role_ids}[$start .. $end];
+        my $filter = 'Role(id) IN (' . join(', ', map { '?' } @slice) . ')';
+        my @tuples = $shrub->GetAll('Role Role2Function Function Function2Feature Feature Feature2Genome', 
+				    "$filter AND (Feature2Genome(to-link) = ?) AND (Function2Feature(security) = ?)", 
+				    [@slice,$g,2],
+				    'Role(id) Function2Feature(to-link) Function(description)');
+        for my $tuple (@tuples) {
+	    my($roleid,$peg,$function) = @$tuple;
+	    $retVal{$roleid}->{$peg} = $function;
+        }
+        # Move to the next chunk.
+        $start = $end + 1;
+    }
+    return \%retVal;
+}
+
+=head3 Locations of Features
+
+    my $fidHash = $helper->fid_locations(\@fids,$just_boundaries);
+
+Return a hash mapping each incoming fid to a location on a contig.
+
+=over 4
+
+=item fids
+
+A reference to a list of Feature ids
+
+=item $just_boundaries ( 1 -> merge segments into one region )
+
+=item RETURN
+
+Returns a reference to a hash mapping each incoming fid to a location.
+
+=back
+
+=cut
+
+sub fid_locations {
+    my ($self, $fids,$just_boundaries) = @_;
+    my $shrub = $self->{shrub};
+    my %retVal;
+
+    # This should build on fid_locs or loc-of, but I do not get it <<<<<<<
+    return \%retVal;
+}
+
 1;
+
